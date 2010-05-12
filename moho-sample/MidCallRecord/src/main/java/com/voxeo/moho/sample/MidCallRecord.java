2c86b6b640afcad8aa4b3e0249cbedac54bd5fd0
Williezhu <wzhu@voxeo.com>
2010-05-12 16:48:05 +0800

    fixed record file name error on windows.

--- moho-sample/MidCallRecord/src/main/java/com/voxeo/moho/sample/MidCallRecord.java
+++ moho-sample/MidCallRecord/src/main/java/com/voxeo/moho/sample/MidCallRecord.java
@@ -61,7 +61,7 @@ public class MidCallRecord implements Application {
       case MATCH:
         URL media = null;
         try {
-          media = new URL(_mediaLocation + System.getProperty("file.separator") + "_" + new Date().getTime()
+          media = new URL(_mediaLocation + System.getProperty("file.separator") + new Date().getTime()
               + "_recording.au");
 
           Recording recording = ((Call) evt.getSource()).getMediaService().record(media);
