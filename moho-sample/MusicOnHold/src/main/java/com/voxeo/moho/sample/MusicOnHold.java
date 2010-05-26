4a42d943083804040b43b52b8bb2877e49b9a8cd
Williezhu <wzhu@voxeo.com>
2010-05-26 12:39:43 +0800

    add music on hold sample.

---
+++ moho-sample/MusicOnHold/src/main/java/com/voxeo/moho/sample/MusicOnHold.java
@@ -0,0 +1,73 @@
+/**
+ * Copyright 2010 Voxeo Corporation
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this
+ * file except in compliance with the License.
+ *
+ * You may obtain a copy of the License at http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software distributed
+ * under the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS
+ * OF ANY KIND, either express or implied. See the License for the specific language
+ * governing permissions and limitations under the License.
+ */
+
+package com.voxeo.moho.sample;
+
+import java.net.MalformedURLException;
+import java.net.URL;
+
+import javax.media.mscontrol.join.Joinable;
+
+import com.voxeo.moho.Application;
+import com.voxeo.moho.ApplicationContext;
+import com.voxeo.moho.Call;
+import com.voxeo.moho.SignalException;
+import com.voxeo.moho.State;
+import com.voxeo.moho.Participant.JoinType;
+import com.voxeo.moho.event.InviteEvent;
+import com.voxeo.moho.event.ReInviteEvent;
+
+public class MusicOnHold implements Application {
+
+  URL _media;
+
+  @Override
+  public void destroy() {
+
+  }
+
+  @Override
+  public void init(final ApplicationContext ctx) {
+    try {
+      String mediaLocation = ctx.getParameter("MediaLocation");
+      if (mediaLocation == null) {
+        throw new IllegalArgumentException();
+      }
+      _media = new URL(mediaLocation);
+    }
+    catch (final MalformedURLException e) {
+      throw new RuntimeException(e);
+    }
+  }
+
+  @State
+  public void handleInvite(final InviteEvent e) {
+    final Call call = e.acceptCall(this);
+    final Call outgoingCall = e.getInvitee().call(e.getInvitor(), null, this);
+    call.join(outgoingCall, JoinType.BRIDGE, Joinable.Direction.DUPLEX);
+  }
+
+  @State
+  public void holdMusic(final ReInviteEvent ev) throws SignalException {
+    final Call c = (Call) ev.source;
+    ev.accept();
+    if (ev.isHold()) {
+      c.getPeers()[0].getMediaService(true).prompt(_media, null, 0);
+    }
+    else {
+      c.join(c.getPeers()[0], JoinType.BRIDGE, Joinable.Direction.DUPLEX);
+    }
+  }
+
+}
