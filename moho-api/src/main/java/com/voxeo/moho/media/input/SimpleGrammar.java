3fe01ed1c56aac8e3da45a5e6643d1e8bdea5a51
kliu <kyle.lzy@gmail.com>
2010-07-15 18:19:02 +0800

    FIxed: #1186463:  Simple Grammar stops working at 309 layer

--- moho-api/src/main/java/com/voxeo/moho/media/input/SimpleGrammar.java
+++ moho-api/src/main/java/com/voxeo/moho/media/input/SimpleGrammar.java
@@ -1,20 +1,19 @@
 /**
- * Copyright 2010 Voxeo Corporation
- *
- * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this
- * file except in compliance with the License.
- *
- * You may obtain a copy of the License at http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software distributed
- * under the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS
- * OF ANY KIND, either express or implied. See the License for the specific language
+ * Copyright 2010 Voxeo Corporation Licensed under the Apache License, Version
+ * 2.0 (the "License"); you may not use this file except in compliance with the
+ * License. You may obtain a copy of the License at
+ * http://www.apache.org/licenses/LICENSE-2.0 Unless required by applicable law
+ * or agreed to in writing, software distributed under the License is
+ * distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied. See the License for the specific language
  * governing permissions and limitations under the License.
  */
 
 package com.voxeo.moho.media.input;
 
+import java.io.UnsupportedEncodingException;
 import java.net.URI;
+import java.net.URLEncoder;
 
 public class SimpleGrammar extends Grammar {
 
@@ -22,9 +21,15 @@ public class SimpleGrammar extends Grammar {
     super(string);
   }
 
+  @SuppressWarnings("deprecation")
   @Override
   public URI toURI() {
-    return null;
+    try {
+      return URI.create("data:" + URLEncoder.encode("application/grammar+voxeo," + toText(), "UTF-8"));
+    }
+    catch (final UnsupportedEncodingException e) {
+      return URI.create("data:" + URLEncoder.encode("application/grammar+voxeo," + toText()));
+    }
   }
 
 }
