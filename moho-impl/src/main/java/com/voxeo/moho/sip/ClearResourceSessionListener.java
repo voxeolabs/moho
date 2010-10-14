72431d00f9200bfcdc1faf2dc9e96990f5863d38
Williezhu <wzhu@voxeo.com>
2010-10-14 12:23:04 +0800

    add clear resource listener to clear resource when sip session is invalidated.

---
+++ moho-impl/src/main/java/com/voxeo/moho/sip/ClearResourceSessionListener.java
@@ -0,0 +1,32 @@
+package com.voxeo.moho.sip;
+
+import javax.servlet.sip.SipSessionEvent;
+import javax.servlet.sip.SipSessionListener;
+import javax.servlet.sip.annotation.SipListener;
+
+import com.voxeo.moho.event.EventSource;
+import com.voxeo.moho.util.SessionUtils;
+
+@SipListener
+public class ClearResourceSessionListener implements SipSessionListener {
+
+  @Override
+  public void sessionCreated(SipSessionEvent sipsessionevent) {
+
+  }
+
+  @Override
+  public void sessionDestroyed(SipSessionEvent sipsessionevent) {
+    final EventSource source = SessionUtils.getEventSource(sipsessionevent.getSession());
+
+    if (source != null && source instanceof SIPCallImpl) {
+      ((SIPCallImpl) source).disconnect(true);
+    }
+  }
+
+  @Override
+  public void sessionReadyToInvalidate(SipSessionEvent sipsessionevent) {
+
+  }
+
+}
