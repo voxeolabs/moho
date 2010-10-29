1c0f85220cd9c66ba04625b8197e10302e4e5c9e
kliu <kyle.lzy@gmail.com>
2010-10-29 15:44:48 +0800

    using the SIP address instead of SIP URI act as source and destination of SIP text event

--- moho-impl/src/main/java/com/voxeo/moho/text/sip/SIPTextEventImpl.java
+++ moho-impl/src/main/java/com/voxeo/moho/text/sip/SIPTextEventImpl.java
@@ -1,10 +1,7 @@
 package com.voxeo.moho.text.sip;
 
 import java.io.IOException;
-import java.io.UnsupportedEncodingException;
 import java.util.Map;
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
 
 import javax.servlet.sip.SipServletRequest;
 import javax.servlet.sip.SipServletResponse;
@@ -40,12 +37,12 @@ public class SIPTextEventImpl extends SIPTextEvent {
 
   @Override
   public TextableEndpoint getSource() {
-    return (TextableEndpoint) _ctx.getEndpoint(_req.getFrom().getURI().toString());
+    return (TextableEndpoint) _ctx.getEndpoint(_req.getFrom().toString());
   }
 
   @Override
   public TextableEndpoint getDestination() {
-    return (TextableEndpoint) _ctx.getEndpoint(_req.getTo().getURI().toString());
+    return (TextableEndpoint) _ctx.getEndpoint(_req.getTo().toString());
   }
 
   @Override
@@ -69,28 +66,6 @@ public class SIPTextEventImpl extends SIPTextEvent {
     catch (final IOException ex) {
       throw new SignalException(ex);
     }
-  }
-
-  private static final Pattern P = Pattern.compile("<[^>]+>([^<]+)<[^>]+>");
-
-  // get text content in a sip request.
-  private String getContent(final SipServletRequest sipReq) throws UnsupportedEncodingException, IOException {
-    String s = sipReq.getContent().toString();
-    if (sipReq.getContentType().equalsIgnoreCase("text/html")) {
-      final Matcher m = P.matcher(s);
-      String a = "";
-      while (m.find()) {
-        a += m.group(1);
-      }
-      s = a;
-    }
-
-    s = s.trim();
-    if (s.endsWith("\r\n")) {
-      s = s.substring(0, s.length() - 2);
-      s = s.trim();
-    }
-    return s;
   }
 
 }
