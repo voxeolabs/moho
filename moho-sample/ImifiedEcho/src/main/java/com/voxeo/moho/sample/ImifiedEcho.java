a353884eea0798f9721bef3fd59f06113fd55745
kliu <kyle.lzy@gmail.com>
2010-10-31 20:41:50 +0800

    specify user name and password of imified account

--- moho-sample/ImifiedEcho/src/main/java/com/voxeo/moho/sample/ImifiedEcho.java
+++ moho-sample/ImifiedEcho/src/main/java/com/voxeo/moho/sample/ImifiedEcho.java
@@ -8,19 +8,28 @@ import com.voxeo.moho.imified.ImifiedEndpoint;
 
 public class ImifiedEcho implements Application {
 
+  private String _name;
+
+  private String _pwd;
+
   public void destroy() {
 
   }
 
   public void init(final ApplicationContext ctx) {
-
+    _name = ctx.getParameter("username");
+    _pwd = ctx.getParameter("password");
   }
 
   @State
   public void handleText(final TextEvent e) throws Exception {
-      final ImifiedEndpoint endpoint = (ImifiedEndpoint) e.getDestination();
-      endpoint.setImifiedUserName("zxpzlp@hotmail.com");
-      endpoint.setImifiedPasswd("wzhu");
-      e.getSource().sendText(endpoint,  e.getText(), e.getTextType());
+    final ImifiedEndpoint endpoint = (ImifiedEndpoint) e.getDestination();
+    if (_name != null) {
+      endpoint.setImifiedUserName(_name);
+      if (_pwd != null) {
+        endpoint.setImifiedPasswd(_pwd);
+      }
+    }
+    e.getSource().sendText(endpoint, e.getText(), e.getTextType());
   }
 }
