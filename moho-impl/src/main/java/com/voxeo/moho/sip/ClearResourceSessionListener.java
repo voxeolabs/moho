58c14d5fc4eedb3936ac14e61841d0da1a217e9d
Williezhu <wzhu@voxeo.com>
2010-11-18 09:46:58 +0800

    update listener to ignore the exception.

--- moho-impl/src/main/java/com/voxeo/moho/sip/ClearResourceSessionListener.java
+++ moho-impl/src/main/java/com/voxeo/moho/sip/ClearResourceSessionListener.java
@@ -4,11 +4,14 @@ import javax.servlet.sip.SipSessionEvent;
 import javax.servlet.sip.SipSessionListener;
 import javax.servlet.sip.annotation.SipListener;
 
+import org.apache.log4j.Logger;
+
 import com.voxeo.moho.event.EventSource;
 import com.voxeo.moho.util.SessionUtils;
 
 @SipListener
 public class ClearResourceSessionListener implements SipSessionListener {
+  private static final Logger LOG = Logger.getLogger(ClearResourceSessionListener.class);
 
   @Override
   public void sessionCreated(SipSessionEvent sipsessionevent) {
@@ -20,7 +23,13 @@ public class ClearResourceSessionListener implements SipSessionListener {
     final EventSource source = SessionUtils.getEventSource(sipsessionevent.getSession());
 
     if (source != null && source instanceof SIPCallImpl) {
-      ((SIPCallImpl) source).disconnect(true);
+      try {
+        ((SIPCallImpl) source).disconnect(true);
+      }
+      catch (Exception ex) {
+        // ignore this exception.
+        LOG.debug("Moho clearing resource exception:" + ex.getMessage());
+      }
     }
   }
 
