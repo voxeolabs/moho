9c8cfc0153cb503240be6ec1c532eb8b7e45631c
Williezhu <wzhu@voxeo.com>
2011-09-24 18:55:00 +0800

    add: moho api implementation based on rayo.

--- moho-presence/src/main/java/com/voxeo/moho/presence/sip/impl/SIPPresenceServiceImpl.java
+++ moho-presence/src/main/java/com/voxeo/moho/presence/sip/impl/SIPPresenceServiceImpl.java
@@ -30,17 +30,17 @@ import com.voxeo.moho.sip.SIPSubscribeEvent.SIPSubscriptionContext;
 import com.voxeo.moho.spi.ExecutionContext;
 
 public class SIPPresenceServiceImpl implements SIPPresenceService {
-  
+
   private final static Logger LOG = Logger.getLogger(SIPPresenceServiceImpl.class);
-  
+
   private ExecutionContext _context;
-  
+
   private SIPPresenceStore _store;
-  
+
   private SipPresenceFactory _presenceFactory;
-  
+
   private NotifyDispatcher _notifyDispatcher;
-  
+
   @Override
   public void init(ExecutionContext context, Map<String, String> props) {
     _context = context;
@@ -78,7 +78,7 @@ public class SIPPresenceServiceImpl implements SIPPresenceService {
   public void doPublish(SIPPublishEvent event) {
     try {
       SipServletRequest req = event.getSipRequest();
-      //validate expire
+      // validate expire
       int expires = req.getExpires();
       if (_presenceFactory.checkPublishExpires(expires) < 0) {
         rejectMinExpire(event, _presenceFactory);
@@ -89,11 +89,12 @@ public class SIPPresenceServiceImpl implements SIPPresenceService {
         content = null;
       }
       StoreHolder.setPresenceStore(_store);
-      //validate supported notifyBodyType
-      //getResource
+      // validate supported notifyBodyType
+      // getResource
       final String resourceKey = Utils.getCleanUri(req.getTo().getURI()).toString();
       EventHeader eventHeader = new EventHeader(req.getHeader("Event"));
-      SIPPresenceResource resource = (SIPPresenceResource) getResource(_presenceFactory, resourceKey, eventHeader.getEventName());
+      SIPPresenceResource resource = (SIPPresenceResource) getResource(_presenceFactory, resourceKey,
+          eventHeader.getEventName());
       String contentType = req.getContentType();
       if (StringUtils.isEmpty(contentType)) {
         contentType = _presenceFactory.getDefaultNotifyBodyName(eventHeader.getEventName());
@@ -103,12 +104,13 @@ public class SIPPresenceServiceImpl implements SIPPresenceService {
       // This is the first publish request
       if (content != null && sipIfMatch == null && expires > 0) {
         NotifyBody notifyBody = _presenceFactory.createNotifyBody(event.getSipRequest());
-        eventSoftState = new SIPEventSoftState(resourceKey, eventHeader.getEventName(), expires, contentType, notifyBody);
+        eventSoftState = new SIPEventSoftState(resourceKey, eventHeader.getEventName(), expires, contentType,
+            notifyBody);
         resource.addEventSoftState(eventSoftState);
         acceptPublish(event, eventSoftState);
       }
       // Publish keepalive
-      else if(content == null && sipIfMatch != null && expires > 0) {
+      else if (content == null && sipIfMatch != null && expires > 0) {
         eventSoftState = resource.getSoftState(sipIfMatch);
         if (eventSoftState != null) {
           eventSoftState.updateExpires(expires);
@@ -164,27 +166,27 @@ public class SIPPresenceServiceImpl implements SIPPresenceService {
     if (isResponsibleFor(event)) {
       try {
         SIPSubscriptionContext subscription = (SIPSubscriptionContext) event.getSubscription();
-        //TODO support resourcelist
-        //verify event
+        // TODO support resourcelist
+        // verify event
         if (subscription.getEventName() == null || !SIPConstans.EVENT_NAME_PRESENCE.equals(subscription.getEventName())) {
           HashMap<String, String> headers = new HashMap<String, String>();
           headers.put("Allow-Events", SIPConstans.EVENT_NAME_PRESENCE);
           event.reject(Reason.BAD_EVENT, headers);
           return;
         }
-        //verify expires
+        // verify expires
         int expire = _presenceFactory.checkSubscribeExpires(subscription.getExpires());
         if (expire < 0) {
           rejectMinExpire(event, _presenceFactory);
         }
-        
+
         _store.startTx();
         StoreHolder.setPresenceStore(_store);
-        //get resource
+        // get resource
         SIPResource resource = getResource(_presenceFactory, subscription.getSubscribee(), subscription.getEventName());
-        
+
         SubscriptionState state = null;
-        //update subscription context
+        // update subscription context
         if (_store.isSubscriptionExist(subscription)) {
           if (subscription.getExpires() == 0) {
             state = resource.removeSubscripton(subscription);
@@ -197,12 +199,12 @@ public class SIPPresenceServiceImpl implements SIPPresenceService {
           state = resource.addSubscription(subscription);
         }
         _store.updateResource(resource);
-        //send 200 OK/202 Accepted before send notify
+        // send 200 OK/202 Accepted before send notify
         if (state == SipSubscriptionStateImpl.ALLOW || state == SipSubscriptionStateImpl.TERMINATED) {
           HashMap<String, String> props = new HashMap<String, String>();
           props.put("Expire", String.valueOf(subscription.getExpires()));
           event.accept(props);
-          
+
           if (LOG.isDebugEnabled()) {
             LOG.debug("Will send notify to " + subscription + " due to subscribe");
           }
@@ -232,7 +234,7 @@ public class SIPPresenceServiceImpl implements SIPPresenceService {
       }
     }
     else {
-      //TODO proxy
+      // TODO proxy
     }
   }
 
@@ -240,26 +242,26 @@ public class SIPPresenceServiceImpl implements SIPPresenceService {
     // TODO check domains.
     return true;
   }
-  
+
   private void invalidateApplicationSession(final SipServletRequest req) {
     if (req.getApplicationSession().isValid()) {
       req.getApplicationSession().invalidate();
     }
   }
-  
+
   private void acceptPublish(SIPPublishEvent event, EventSoftState eventSoftState) {
     HashMap<String, String> headers = new HashMap<String, String>();
     headers.put("SIP-ETag", eventSoftState.getEntityTag());
     headers.put("Expires", String.valueOf(eventSoftState.getSpareTime()));
     event.accept(headers);
   }
-  
+
   private void rejectMinExpire(AcceptableEvent event, SipPresenceFactory presenceFactory) {
     HashMap<String, String> headers = new HashMap<String, String>();
     headers.put("Min-Expires", String.valueOf(presenceFactory.getSubscribeMinExpires()));
     event.reject(Reason.INTERVAL_TOO_BRIEF, headers);
   }
-  
+
   private SIPResource getResource(SipPresenceFactory presenceFactory, String resourceUri, String eventName) {
     SIPResource resource = (SIPResource) _store.getResource(resourceUri, eventName);
     if (resource == null) {
@@ -271,21 +273,25 @@ public class SIPPresenceServiceImpl implements SIPPresenceService {
     }
     return resource;
   }
-  
+
   @Override
   public void destroy() {
     _context = null;
-    _notifyDispatcher.shutdown();
+    if (_notifyDispatcher != null) {
+      _notifyDispatcher.shutdown();
+    }
     _notifyDispatcher = null;
     _presenceFactory = null;
-    _store.destroy();
+    if (_store != null) {
+      _store.destroy();
+    }
     _store = null;
   }
 
   @Override
   public void run() {
-    //TODO EventSoftState clean
-    //TODO Subscription clean
+    // TODO EventSoftState clean
+    // TODO Subscription clean
   }
 
   public NotifyDispatcher getNotifyDispatcher() {
