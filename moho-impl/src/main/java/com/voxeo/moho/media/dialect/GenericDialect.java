144275cc366e749130ab6303fa013f530852542f
Williezhu <wzhu@voxeo.com>
2011-09-27 11:48:09 +0800

    Modified moho to use different language parameters for play and recognizer.

--- moho-impl/src/main/java/com/voxeo/moho/media/dialect/GenericDialect.java
+++ moho-impl/src/main/java/com/voxeo/moho/media/dialect/GenericDialect.java
@@ -6,31 +6,43 @@ import com.voxeo.moho.media.InputMode;
 
 public class GenericDialect implements MediaDialect {
 
-    @Override
-    public void setBeepOnConferenceEnter(Parameters parameters, Boolean value) {}
+  @Override
+  public void setBeepOnConferenceEnter(Parameters parameters, Boolean value) {
+  }
 
-    @Override
-    public void setBeepOnConferenceExit(Parameters parameters, Boolean value) {}
+  @Override
+  public void setBeepOnConferenceExit(Parameters parameters, Boolean value) {
+  }
 
-    @Override
-    public void setSpeechInputMode(Parameters parameters, InputMode value) {}
+  @Override
+  public void setSpeechInputMode(Parameters parameters, InputMode value) {
+  }
 
-    @Override
-    public void setSpeechLanguage(Parameters parameters, String value) {}
+  @Override
+  public void setSpeechLanguage(Parameters parameters, String value) {
+  }
 
-    @Override
-    public void setSpeechTermChar(Parameters parameters, Character value) {}
+  @Override
+  public void setSpeechTermChar(Parameters parameters, Character value) {
+  }
 
-    @Override
-    public void setTextToSpeechVoice(Parameters parameters, String value) {}
+  @Override
+  public void setTextToSpeechVoice(Parameters parameters, String value) {
+  }
 
-    @Override
-    public void setDtmfHotwordEnabled(Parameters parameters, Boolean value) {}
+  @Override
+  public void setDtmfHotwordEnabled(Parameters parameters, Boolean value) {
+  }
 
-    @Override
-    public void setDtmfTypeaheadEnabled(Parameters parameters, Boolean value) {}
+  @Override
+  public void setDtmfTypeaheadEnabled(Parameters parameters, Boolean value) {
+  }
 
-    @Override
-    public void setConfidence(Parameters parameters, float value) {}
+  @Override
+  public void setConfidence(Parameters parameters, float value) {
+  }
 
+  @Override
+  public void setTextToSpeechLanguage(Parameters parameters, String value) {
+  }
 }
