571c288390cc6cf37a58fafa53750ca4e99e30c7
Williezhu <wzhu@voxeolabs.com>
2013-01-04 18:17:33 +0800

    Revert "create request when creating outgoing call. so moho app can get sip invite after created outoing cal."
    This reverts commit 42b35d15254ec99661bd7450dd2b54fae34d0b72.

--- moho-impl/src/main/java/com/voxeo/moho/sip/SIPCallImpl.java
+++ moho-impl/src/main/java/com/voxeo/moho/sip/SIPCallImpl.java
@@ -841,15 +841,9 @@ public abstract class SIPCallImpl extends CallImpl implements SIPCall, MediaEven
         try {
           if (this instanceof SIPOutgoingCall) {
             if (_invite != null && (_invite.getSession().getState() == SipSession.State.EARLY || _invite.getSession().getState() == SipSession.State.INITIAL)) {
-              try{
-                SipServletRequest cancelRequest = _invite.createCancel();
-                SIPHelper.addHeaders(cancelRequest, headers);
-                cancelRequest.send();
-              }
-              catch(Exception ex){
-                LOG.warn("Exception when disconnecting failed outbound call:"+ ex.getMessage());
-                _invite.getSession().invalidate();
-              }
+              SipServletRequest cancelRequest = _invite.createCancel();
+              SIPHelper.addHeaders(cancelRequest, headers);
+              cancelRequest.send();
             }
           }
           else if (this instanceof SIPIncomingCall) {
--- moho-impl/src/main/java/com/voxeo/moho/sip/SIPOutgoingCall.java
+++ moho-impl/src/main/java/com/voxeo/moho/sip/SIPOutgoingCall.java
@@ -47,8 +47,6 @@ public class SIPOutgoingCall extends SIPCallImpl implements OutgoingCall {
     _address = to;
     _from = from;
     _headers = headers;
-    
-    createRequest();
   }
 
   @Override
