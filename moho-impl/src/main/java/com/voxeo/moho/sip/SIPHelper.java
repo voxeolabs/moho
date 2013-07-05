ae945135137ab321eb44e7680f990efd9fedf4be
Williezhu <wzhu@voxeolabs.com>
2013-07-05 19:09:02 +0800

    pass the From and To address.

--- moho-impl/src/main/java/com/voxeo/moho/sip/SIPHelper.java
+++ moho-impl/src/main/java/com/voxeo/moho/sip/SIPHelper.java
@@ -60,8 +60,16 @@ public class SIPHelper {
     SipServletRequest req = null;
     if (origRequest != null) {
       LOG.debug("Continue routing from orig req:" + origRequest);
+      Map<String, List<String>> headerMap = new HashMap<String, List<String>>();
+      List<String> fromList = new ArrayList<String>();
+      fromList.add(from.toString());
+      headerMap.put("From", fromList);
+      
+      List<String> toList = new ArrayList<String>();
+      toList.add(to.toString());
+      headerMap.put("To", toList);
+      
       if (!Utils.isCopyHeadersForContinueRouting(appContext)) {
-        Map<String, List<String>> headerMap = new HashMap<String, List<String>>();
         headerMap.put(Constants.PrismB2BUADoNotCopyHeader, new ArrayList<String>());
         try {
           req = origRequest.getB2buaHelper().createRequest(origRequest, true, headerMap);
@@ -78,7 +86,12 @@ public class SIPHelper {
         }
       }
       else {
-        req = origRequest.getB2buaHelper().createRequest(origRequest);
+        try{
+          req = origRequest.getB2buaHelper().createRequest(origRequest, true, headerMap);
+        }
+        catch (final TooManyHopsException e) {
+          throw new RuntimeException(e);
+        }
       }
 
       try {
