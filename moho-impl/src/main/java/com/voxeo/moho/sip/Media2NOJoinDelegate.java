6a72631cebb9cac21cbe3a05390a3d292a4cdf06
Williezhu <wzhu@voxeolabs.com>
2013-11-04 13:53:36 +0800

    fix MOHO-94 failed to acceptWithEarlymeida when 183 withSDP followed by 180 w/o SDP.

--- moho-impl/src/main/java/com/voxeo/moho/sip/Media2NOJoinDelegate.java
+++ moho-impl/src/main/java/com/voxeo/moho/sip/Media2NOJoinDelegate.java
@@ -155,10 +155,22 @@ public class Media2NOJoinDelegate extends JoinDelegate {
           }
 
           _call1.setSIPCallState(SIPCall.State.PROGRESSED);
+          _call1.notify();
 
-          synchronized (this) {
-            this.notify();
+          if (_earlyMediaResponse != null) {
+            SipServletResponse resp = _earlyMediaResponse;
+            _earlyMediaResponse = null;
+            try {
+              SIPHelper.trySendPrack(resp);
+            }
+            catch (IOException e) {
+              LOG.error("Exception when sending back PRACK", e);
+              Exception ex = new NegotiateException(event);
+              done(Cause.ERROR, ex);
+              _call1.fail(ex);
+            }
           }
+
         }
         return;
       }
@@ -179,15 +191,20 @@ public class Media2NOJoinDelegate extends JoinDelegate {
       if (SIPHelper.isProvisionalResponse(res)) {
         _call1.setSIPCallState(SIPCall.State.ANSWERING);
 
-        if (SIPHelper.getRawContentWOException(res) != null && SIPHelper.needPrack(res) && _earlyMediaResponse == null) {
+        if (SIPHelper.getRawContentWOException(res) != null && SIPHelper.needPrack(res)) {
+          if (_earlyMediaResponse != null) {
+            return;
+          }
+
           _earlyMediaResponse = res;
           _call1.setSIPCallState(SIPCall.State.PROGRESSING);
           if (!processedAnswer) {
             processedAnswer = true;
             _call1.processSDPAnswer(res);
           }
-
-          SIPHelper.trySendPrack(res);
+          else {
+            SIPHelper.trySendPrack(res);
+          }
         }
         else {
           SIPHelper.trySendPrack(res);
